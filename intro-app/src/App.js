a3f27d4d413103128e8be691e6d1f648e085d619
macabdul9 <abdulwaheed1513@gmail.com>
2020-01-26 02:18:11 +0530

    "vs code commit"

--- intro-app/src/App.js
+++ intro-app/src/App.js
@@ -5,10 +5,15 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div>
+        <div className="App">
         <h1>I am react</h1>
         <p>This app is working</p>
         <p>Hello there</p>
+      </div>
+      <div className='App'>
+        <p>I am new Component</p>
+      </div>
       </div>
     );
   }
---
+++ intro-app/src/Person/Person.js
@@ -0,0 +1,9 @@
+// A component is just a javascript function which returns some JSX
+
+person = () =>{
+    return (
+        <div>
+            <h1></h1>
+        </div>
+    )
+}
