f566dab90c587e4245631ea2b630656557a861fb
macabdul9 <abdulwaheed1513@gmail.com>
2020-01-26 21:30:16 +0530

    add files via terminal

--- intro-app/src/App.js
+++ intro-app/src/App.js
@@ -2,18 +2,42 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// Import our own component
+import Person from './Person/Person'
 class App extends Component {
+  // states
+  state = {
+    persons : [
+      {name:"Abdul", age:21},
+      {name:"Waheed", age:22},
+      {name:"Mac", age:20}
+    ]
+  }
+
+  switchNameHandler =()=>{
+    // console.log('was clicked')
+    // this.state.persons[0].name="Robert"  this will not work to change the state see below
+    this.setState({persons : [
+      {name:"Andrew", age:21},
+      {name:"Viola", age:22},
+      {name:"Max", age:20}
+    ]})
+  }
   render() {
     return (
-      <div>
-        <div className="App">
+      <div className='App'>
+        <div >
         <h1>I am react</h1>
         <p>This app is working</p>
         <p>Hello there</p>
       </div>
-      <div className='App'>
+      <div>
         <p>I am new Component</p>
       </div>
+      <button onClick={this.switchNameHandler}>Switch Name</button>
+      <Person name={this.state.persons[0].name } age={this.state.persons[0].age }></Person>
+      <Person name={this.state.persons[1].name } age={this.state.persons[1].age }>I love football</Person>
+      <Person name={this.state.persons[2].name } age={this.state.persons[2].age }></Person>
       </div>
     );
   }
--- intro-app/src/Person/Person.js
+++ intro-app/src/Person/Person.js
@@ -1,9 +1,12 @@
 // A component is just a javascript function which returns some JSX
-
-person = () =>{
+import React from 'react';
+const person = (props) =>{
     return (
         <div>
-            <h1></h1>
+            <h1>I am {props.name}, I am {props.age} years old </h1>
+            <p>{props.children}</p>
         </div>
     )
 }
+
+export default person;
